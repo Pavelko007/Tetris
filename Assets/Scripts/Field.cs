16f9da907ae55eaa3f1598c6e84e1354257ef7ef
Pavlo Kvas <kvaspasha@gmail.com>
2019-11-16 16:51:44 +0200

    draw field gizmo

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        var colorBefore = Gizmos.color;
+        Gizmos.color = Color.blue;
+
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                var startPos = Vector2.zero + Vector2.down*i + Vector2.right*j;
+
+                Gizmos.DrawWireCube(startPos, Vector3.one);
+            }
+        }
+        
+
+        Gizmos.color = colorBefore;
+    }
+
+
+}
