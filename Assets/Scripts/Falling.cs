2eb183146419a7d4887bdd9b67e680991e1cdf30
Pavlo Kvas <kvaspasha@gmail.com>
2019-11-27 10:12:08 +0200

    add falling script

---
+++ Assets/Scripts/Falling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public float fallingSpeed = 1f;
+    private float fallingTime = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        fallingTime += Time.deltaTime;
+        if (fallingTime > 1 / fallingSpeed)
+        {
+            fallingTime = 0;
+            StepDown();
+        }
+    }
+
+    private void StepDown()
+    {
+        transform.Translate(Vector3.down);
+    }
+}
